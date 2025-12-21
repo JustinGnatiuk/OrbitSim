ca851533e65bed5263ac3638f04c4d1c3e025764
JustinGnatiuk <JustinGnatiuk@gmail.com>
2025-12-21 03:27:22 -0500

    refactor object placement

--- celestialobject.py
+++ celestialobject.py
@@ -9,27 +9,35 @@ class Vector2:
 
 class CelestialObject:
     def __init__(self, origin: Vector2, canvas: Canvas, radius: int, tag: str,) -> None:
+        self.center = Vector2(origin.x, origin.y)
         self.canvas = canvas
         self.radius = radius
         self.color = "white"
         self.tag = tag
 
     def draw(self):
-        return
+        x1 = self.center.x - self.radius
+        y1 = self.center.y - self.radius
+        x2 = self.center.x + self.radius
+        y2 = self.center.y + self.radius
+        self.canvas.create_oval(x1, y1, x2, y2, fill="black", outline=self.color)
+
 
 
 class ObjectManager:
     def __init__(self, canvas: Canvas) -> None:
         self.canvas = canvas
-        self.celestialobjects = []
+        self.celestialObjects = []
 
         self.canvas.bind("<Button-1>", self.spawn_object)
 
     # Spawn Celestial Object ( a Circle )
     def spawn_object(self, event):
-        radius = 10
-        x1 = event.x - radius
-        y1 = event.y - radius
-        x2 = event.x + radius
-        y2 = event.y + radius
-        self.canvas.create_oval(x1, y1, x2, y2, fill="black", outline="white")
+        new_object = CelestialObject(
+            Vector2(event.x, event.y), 
+            self.canvas,
+            10,
+            "object"
+        )
+        self.celestialObjects.append(new_object)
+        new_object.draw()
