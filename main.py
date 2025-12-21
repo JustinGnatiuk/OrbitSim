dc7fd35c5c52015b3559525ba74de795f52ecd96
JustinGnatiuk <JustinGnatiuk@gmail.com>
2025-12-20 22:25:39 -0500

    more GUI config

--- main.py
+++ main.py
@@ -8,15 +8,29 @@ class OrbitSimulation:
     def __init__(self,root):
         self.root = root
         self.root.title("Orbit Simulation - github.com/JustinGnatiuk")
+        self.root.resizable(0,0)
+        self.root.minsize(WIDTH,HEIGHT)
+        self.root.maxsize(WIDTH,HEIGHT)
         self.canvas = None
         self.orbit_manager = None
 
         self.build_gui()
 
     def build_gui(self):
-        content = ttk.Frame(self.root)
 
-        self.canvas = Canvas(content, width=WIDTH, height=HEIGHT, bg="black")
+        # Create Main Container
+        main_container = ttk.Frame(self.root, width=WIDTH, height=HEIGHT)
+        main_container.pack(fill='both', expand=False)
+        main_container.pack_propagate(False)
+
+        # Create Simulation Canvas
+        self.canvas = Canvas(main_container, width=WIDTH-250, height=HEIGHT, bg='black')
+        self.canvas.pack(side='left', fill='both', expand=True)
+        
+        # Create Side Configuration Bar
+        side_config = ttk.Frame(main_container, width=250)
+        side_config.pack(side='right', fill='y', expand=False)
+        side_config.pack_propagate(False)
 
     def start(self):
         if self.canvas is None:
